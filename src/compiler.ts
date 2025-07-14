b0735fff2f665c830f6e529acf1c894231349cce
arobsn <87387688+arobsn@users.noreply.github.com>
2025-07-14 20:35:19 -0300

    bum packages

--- src/compiler.ts
+++ src/compiler.ts
@@ -14,7 +14,7 @@ export async function compileScript(filename: string, flags: CompilerFlags): Pro
 
   const sigmaCompiler = await getSigmaCompiler();
   const enc = parseEncoding(flags.encoding);
-  const version = parseErgoTreeVersion(flags.ergotreeVersion) as 0 | 1; // todo: support v2 and v3 in fleet
+  const version = parseErgoTreeVersion(flags.ergotreeVersion) as 0 | 1;
   const commonOptions = {
     network: parseNetwork(flags.network),
     segregateConstants: flags.constSegregation
